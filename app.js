5f0d8134bb342083ccb0049026faba53ea058794
Shinichi <miyakawashi@gmail.com>
2021-12-19 20:37:52 -0500

    command line function to return html template

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+
+
+
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name3, github] = profileDataArgs;
 
+const generatePage = (userName, githubName) => {
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
 
-    console.log("============================");
+    <body>
+        <h1>${userName}</h1>
+        <h2><a href='https://github.com/${githubName}'>Github</a></h2>
+    </body>
+    </html>
+    `;
+}
 
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-};
-
-printProfileData(profileDataArgs)
+console.log(name3, github);
+console.log(generatePage(name3, github));
