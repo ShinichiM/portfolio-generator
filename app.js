7bded7c376f16d1e945d0886f296b4e157bf19cc
Shinichi <miyakawashi@gmail.com>
2021-12-21 20:44:34 -0500

    basic html template creation

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-
-
-
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+const profileDataArgs = process.argv.slice(2);
 const [name3, github] = profileDataArgs;
 
-const generatePage = (userName, githubName) => {
 
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
 
-    <body>
-        <h1>${userName}</h1>
-        <h2><a href='https://github.com/${githubName}'>Github</a></h2>
-    </body>
-    </html>
-    `;
-}
+fs.writeFile('./index.html', generatePage(name3, github), err => {
+  if (err) throw err;
 
-console.log(name3, github);
-console.log(generatePage(name3, github));
+  console.log('Portfoilio complete! Check out index.html to see the output!')
+})
