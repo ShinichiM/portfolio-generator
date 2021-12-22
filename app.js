ff0fbde467ca457a3d1cf015407e59fa9f747e23
Shinichi <miyakawashi@gmail.com>
2021-12-22 12:47:59 -0500

    project prompting

--- app.js
+++ app.js
@@ -17,8 +17,61 @@ const promptUser = () => {
       type: 'input',
       name: 'name',
       message: 'What is your name?' 
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information yourself:'
     }
   ])
 }
 
-promptUser().then(answers => console.log(answers))
+const promptProject = () => {
+ console.log(`
+ =================
+ Add a New Project
+ =================
+ `); 
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (REQUIRED)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check All that Apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (REQUIRED)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type:'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default:'false'
+    }
+  ])
+}
+
+promptUser().then(answers => console.log(answers)).then(promptProject).then(projectAnswers => console.log(projectAnswers))
